1fa0db069f694855ff2a1eea375198f42b46b667
Pavanjadhav1 <94003345+Pavanjadhav1@users.noreply.github.com>
2023-03-01 00:46:43 +0530

    Update SpringbootSession7Application.java

--- src/main/java/com/example/demo/SpringbootSession7Application.java
+++ src/main/java/com/example/demo/SpringbootSession7Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+@Controller
 @RestController
 @SpringBootApplication
 public class SpringbootSession7Application {
